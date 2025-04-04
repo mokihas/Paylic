9258dac155f8bf4ebb77eb8b390b5f50c4622b0d
Mokibul Hassan <115884365+mokihas@users.noreply.github.com>
2025-04-04 15:34:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '/pages/Home';
-import About from '/pages/About';
-import Contact from '/pages/Contact';
-import Header from '/components/Header';
-import Footer from '/components/Footer';
+import React, { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import './index.css'
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
+
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <Header />
+    <BrowserRouter>
+      <div className={`min-h-screen flex flex-col ${darkMode ? 'dark bg-gray-900' : 'bg-gray-50'}`}>
+        <Header darkMode={darkMode} setDarkMode={setDarkMode} />
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -20,8 +23,8 @@ function App() {
         </main>
         <Footer />
       </div>
-    </Router>
-  );
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
