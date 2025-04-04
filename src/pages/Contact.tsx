9e2e26d1f9bf46159cfac9e9986fc4ac1cf2026c
Mokibul Hassan <115884365+mokihas@users.noreply.github.com>
2025-04-04 15:26:18 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '/components/Header';
+import Footer from '/components/Footer';
 import { FiMail, FiPhone, FiMapPin, FiSend } from 'react-icons/fi';
 
 const Contact = () => {
